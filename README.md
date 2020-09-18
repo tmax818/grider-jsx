0cf5240eafcfb3fcb9bdacd3e6e4dde5dd79bac1
tmax818 <tylermaxwell661@gmail.com>
2020-09-18 07:52:20 -0700

    end

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - all about imports
 
 # 11-lec
+
+- what is a component?
+  - a function or a class
+  - its purpose is to;
+    - produce HTML
+    - handle feedback from user (clicks, typing, dragging, etc.)
--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
 // take the react component and render it to the screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
