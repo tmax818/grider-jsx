37f693d491f6b5b82a8ba346ee3eea168b201cb0
tmax818 <tylermaxwell661@gmail.com>
2020-09-18 07:48:03 -0700

    update readme and index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+
+// take the react component and render it to the screen
